01c4a1222d1cc94722e12395d0deb7cc65ff70fc
PedroUria <43831167+PedroUria@users.noreply.github.com>
2020-02-22 07:03:29 -0800

    More stuff

--- nndesign/Linear_transformations.py
+++ nndesign/Linear_transformations.py
@@ -11,6 +11,27 @@ from nndesign_layout import NNDLayout
 
 from get_package_path import PACKAGE_PATH
 
+# v1 = v11 * e1 + v12 * e2
+# v2 = v21 * e1 + v22 * e2
+#
+# y1 = y11 * e1 + y12 * e2
+# y2 = y21 * e1 + y22 * e2
+#
+# T(v1) = y1 —> v11 * T(e1) + v12 * T(e2) = y11 * e1 + y12  * e2
+# T(v2) = y2 —> v21 * T(e1) + v22 * T(e2) = y21 * e1 + y22 * e2
+#               ----------------------------——————————————————————
+# 			T(e1) = ( y11 * e1 + y12  * e2 - v12 * T(e2) ) / v11 = ( y21 * e1 + y22 * e2 - v22 * T(e2) ) / v21 —>
+# 			—> - v12 * T(e2) = (v11 / v21)  * ( y21 * e1 + y22 * e2 - v22 * T(e2) ) - y11 * e1 + y12  * e2 —>
+# 			—> T(e2) * (-v12 + v22 * v11 / v21) = (v11 / v21)  * ( y21 * e1 + y22 * e2) - y11 * e1 + y12  * e2 —>
+# 			—> T(e2) = ( ( v11 * y21 / v21 - y11 ) * e1 + ( v11 * y22 / v21 + y12 ) * e2 ) / (-v12 + v22 * v11 / v21)
+# 			—> T(e1) = ( y11 - v12 * ( v11 * y21 / v21 - y11 ) / (-v12 + v22 * v11 / v21) ) * e1 +
+# 						+ ( y12 - v12 * ( v11 * y22 / v21 + y12 ) / (-v12 + v22 * v11 / v21) ) * e2
+#
+# A_div = -v12 + v22 * v11 / v21
+#
+# A = [ T(e1) T(e2) ] = [  (y11 - v12 * ( v11 * y21 / v21 - y11 ) / A_div) / v11    ( v11 * y21 / v21 - y11 ) / A_div   ]
+# 				 [  (y12 - v12 * ( v11 * y22 / v21 + y12 ) /A_div) / v11     ( v11 * y22 / v21 + y12 ) / A_div ]
+
 
 class LinearTransformations(NNDLayout):
     def __init__(self, w_ratio, h_ratio):
@@ -20,6 +41,8 @@ class LinearTransformations(NNDLayout):
                           PACKAGE_PATH + "Chapters/2/Logo_Ch_2.svg", PACKAGE_PATH + "Chapters/2/nn2d1.svg", show_pic=False)
 
         self.cid1, self.cid2 = None, None
+        self.cid3 = None
+        self.A, self.eig_v1, self.eig_v2 = None, None, None
 
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
@@ -34,9 +57,8 @@ class LinearTransformations(NNDLayout):
         self.axes_1.set_title("Original Vectors")
         self.axes_1.set_xlim(-1, 1)
         self.axes_1.set_ylim(-1, 1)
-        self.line, = self.axes_1.plot([], linestyle="-", color="gray")
+        self.line, = self.axes_1.plot([], linestyle="--", color="gray")
         self.line_data_x, self.line_data_y = [], []
-        # self.line2, = self.axes_1.plot([], linestyle="-", label="line2")
         self.line2 = None
         self.line_data_x2, self.line_data_y2 = [], []
         self.canvas.draw()
@@ -58,7 +80,38 @@ class LinearTransformations(NNDLayout):
         self.line2, = self.axes_2.plot([], linestyle="--")
         self.line_data_x2, self.line_data_y2 = [], []
         self.canvas2.draw()
-        # self.canvas2.mpl_connect('button_press_event', self.on_mouseclick)
+        self.canvas2.mpl_connect('button_press_event', self.on_mouseclick2)
+
+        self.run_button = QtWidgets.QPushButton("Clear", self)
+        self.run_button.setStyleSheet("font-size:13px")
+        self.run_button.setGeometry(self.x_chapter_button * self.w_ratio, 420 * self.h_ratio,
+                                    self.w_chapter_button * self.w_ratio, self.h_chapter_button * self.h_ratio)
+        self.run_button.clicked.connect(self.on_run)
+
+        self.on_run()
+
+    def on_run(self):
+        if self.cid3:
+            self.canvas2.mpl_disconnect(self.cid3)
+        if self.cid1:
+            self.canvas.mpl_disconnect(self.cid1)
+        if self.cid2:
+            self.canvas.mpl_disconnect(self.cid2)
+        self.vectors = []
+        self.line_data_x, self.line_data_y = [], []
+        while self.axes_1.lines:
+            self.axes_1.lines.pop()
+        while self.axes_1.collections:
+            for collection in self.axes_1.collections:
+                collection.remove()
+        self.line, = self.axes_1.plot([], linestyle="--", color="gray")
+        self.line2, = self.axes_2.plot([], linestyle="--")
+        self.line_data_x2, self.line_data_y2 = [], []
+        while self.axes_2.collections:
+            for collection in self.axes_2.collections:
+                collection.remove()
+        self.canvas.draw()
+        self.canvas2.draw()
 
     def on_mouseclick(self, event):
         if event.xdata != None and event.xdata != None:
@@ -80,7 +133,20 @@ class LinearTransformations(NNDLayout):
             elif n_vectors == 4:
                 self.axes_1.quiver([0], [0], [event.xdata], [event.ydata], units="xy", scale=1, label="Transformed 2", color="r")
                 self.canvas.mpl_disconnect(self.cid2)
-                # TODO: Add tranformation stuff here
+                v1, y1, v2, y2 = self.vectors[0], self.vectors[1], self.vectors[2], self.vectors[3]
+                A_div = (-v1[1] + v2[1] * v1[0] / v2[0])
+                A_11 = (y1[0] - v1[1] * (v1[0] * y2[0] / v2[0] - y1[0]) / A_div) / v1[0]
+                A_12 = (v1[0] * y2[0] / v2[0] - y1[0]) / A_div
+                A_21 = (y1[1] - v1[1] * (v1[0] - y2[1] / v2[0] + y1[1]) / A_div) / v1[0]
+                A_22 = (v1[0] + y2[1] / v2[0] + y1[1]) / A_div
+                self.A = np.array([[A_11, A_12], [A_21, A_22]])
+                e, v = np.linalg.eig(self.A)
+                if "complex" in str(v.dtype):
+                    print("Complex!!!")
+                else:
+                    self.axes_2.quiver([0], [0], [-v[0, 0]], [-v[0, 1]], units="xy", scale=1, label="Eigenvector 1", color="g")
+                    self.axes_2.quiver([0], [0], [v[1, 0]], [v[1, 1]], units="xy", scale=1, label="Eigenvector 2", color="g")
+                self.canvas2.draw()
             else:
                 return
             self.canvas.draw()
@@ -101,8 +167,29 @@ class LinearTransformations(NNDLayout):
             while self.axes_1.lines:
                 self.axes_1.lines.pop()
             self.axes_1.lines.append(self.line)
-            self.axes_1.plot(self.line_data_x2, self.line_data_y2, linestyle="-", color="gray")
-            # self.line2.set_data(self.line_data_x2, self.line_data_y2)
+            self.axes_1.plot(self.line_data_x2, self.line_data_y2, linestyle="--", color="gray")
             self.canvas.draw()
             self.line_data_x2.pop()
             self.line_data_y2.pop()
+
+    def on_mouseclick2(self, event):
+        if event.xdata != None and event.xdata != None and len(self.vectors) == 4:
+            if self.axes_2.collections:
+                while len(self.axes_2.collections) > 2:
+                    self.axes_2.collections[-1].remove()
+            if self.cid3:
+                self.canvas2.mpl_disconnect(self.cid3)
+            self.axes_2.quiver([0], [0], [event.xdata], [event.ydata], units="xy", scale=1)
+            v_transformed = np.dot(self.A, np.array([[event.xdata], [event.ydata]]))
+            self.axes_2.quiver([0], [0], [v_transformed[0, 0]], [v_transformed[1, 0]], units="xy", scale=1, color="g")
+            self.cid3 = self.canvas2.mpl_connect("motion_notify_event", self.on_mousepressed3)
+            self.canvas2.draw()
+
+    def on_mousepressed3(self, event):
+        if event.xdata != None and event.ydata != None:
+            self.axes_2.collections[-1].remove()
+            self.axes_2.collections[-1].remove()
+            self.axes_2.quiver([0], [0], [event.xdata], [event.ydata], units="xy", scale=1)
+            v_transformed = np.dot(self.A, np.array([[event.xdata], [event.ydata]]))
+            self.axes_2.quiver([0], [0], [v_transformed[0, 0]], [v_transformed[1, 0]], units="xy", scale=1, color="g")
+            self.canvas2.draw()
