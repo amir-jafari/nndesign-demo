d2dbd59350ade102aad1a754bde785e32f4af3a4
PedroUria <43831167+PedroUria@users.noreply.github.com>
2020-03-30 14:27:19 -0400

    Demo finished

--- nndesign/Gram_Schmidt.py
+++ nndesign/Gram_Schmidt.py
@@ -14,66 +14,72 @@ from get_package_path import PACKAGE_PATH
 
 class GramSchmidt(NNDLayout):
     def __init__(self, w_ratio, h_ratio):
-        super(GramSchmidt, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot=False, create_two_plots=True)
+        super(GramSchmidt, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot=False)
 
-        self.fill_chapter("Gram-Schmidt", 5, " The first orthogonal vector v1,\n is chosen to be the first original\n"
-                          " vector, y1. \n \n To obtain the second orthogonal\n vector, v2, projection of y2 onto\n the"
-                          " first orthogonal vector\n, v1, is subtracted from y2.\n \n Click start to begin.",
-                          PACKAGE_PATH + "Logo/Logo_Ch_5.svg", PACKAGE_PATH + "Chapters/2/nn2d1.svg", show_pic=False)
+        self.fill_chapter("Gram-Schmidt", 5, "Click [Start] to begin.\n\nClick twice in the top graph\nto create two vectors"
+                                             "to be\northogonalized. Then click\n[Compute] to see the\northogonal vectors.\n\n"
+                                             "Click [Start] again to repeat.",
+                          PACKAGE_PATH + "Logo/Logo_Ch_5.svg", None)
+
+        self.make_plot(1, (120, 120, 270, 270))
+        self.make_plot(2, (120, 390, 270, 270))
 
         self.axes_1 = self.figure.add_subplot(1, 1, 1)
         self.axes_1.set_title("Original Vectors", fontdict={'fontsize': 10})
-        self.axes_1.set_xlim(-2, 2)
-        self.axes_1.set_ylim(-2, 2)
+        self.axes_1.set_xlim(-1.2, 1.2)
+        self.axes_1.set_ylim(-1.2, 1.2)
         self.axes1_points = []
         self.axes1_v1 = self.axes_1.quiver([0], [0], [0], [0], units="xy", scale=1)
         self.axes1_v2 = self.axes_1.quiver([0], [0], [0], [0],  units="xy", scale=1)
         self.axes1_proj = self.axes_1.quiver([0], [0], [0], [0],  units="xy", scale=1, headlength=0, headwidth=0, headaxislength=0, linestyle='dashed', color="red")
         self.text1, self.text2 = None, None
+        self.text_start = self.axes_1.text(-0.7, -0.075, "<CLICK ON ME>")
         self.axes1_proj_line, = self.axes_1.plot([], "--")
         self.axes1_proj_line.set_color("red")
-        self.axes_1.set_xticks([-2, -1, 0, 1])
-        self.axes_1.set_yticks([-2, -1, 0, 1])
-        self.axes_1.set_xlabel("$x$")
-        self.axes_1.xaxis.set_label_coords(1, -0.025)
-        self.axes_1.set_ylabel("$y$")
-        self.axes_1.yaxis.set_label_coords(-0.025, 1)
+        self.axes_1.plot([0] * 20, np.linspace(-1.2, 1.2, 20), linestyle="dashed", linewidth=0.5, color="gray")
+        self.axes_1.plot(np.linspace(-1.2, 1.2, 20), [0] * 20, linestyle="dashed", linewidth=0.5, color="gray")
+        # self.axes_1.set_xticks([-2, -1, 0, 1])
+        # self.axes_1.set_yticks([-2, -1, 0, 1])
+        # self.axes_1.set_xlabel("$x$")
+        # self.axes_1.xaxis.set_label_coords(1, -0.025)
+        # self.axes_1.set_ylabel("$y$")
+        # self.axes_1.yaxis.set_label_coords(-0.025, 1)
         self.canvas.draw()
         self.canvas.mpl_connect('button_press_event', self.on_mouseclick1)
 
         self.axes_2 = self.figure2.add_subplot(1, 1, 1)
         self.axes_2.set_title("Orthogonalized Vectors", fontdict={'fontsize': 10})
-        self.axes_2.set_xlim(-2, 2)
-        self.axes_2.set_ylim(-2, 2)
+        self.axes_2.set_xlim(-1.2, 1.2)
+        self.axes_2.set_ylim(-1.2, 1.2)
         self.axes2_v1 = self.axes_2.quiver([0], [0], [0], [0], units="xy", scale=1, color="g")
         self.axes2_v2 = self.axes_2.quiver([0], [0], [0], [0], units="xy", scale=1, color="g")
+        self.axes_2.plot([0] * 20, np.linspace(-1.2, 1.2, 20), linestyle="dashed", linewidth=0.5, color="gray")
+        self.axes_2.plot(np.linspace(-1.2, 1.2, 20), [0] * 20, linestyle="dashed", linewidth=0.5, color="gray")
         self.text3, self.text4 = None, None
-        self.axes_2.set_xticks([-2, -1, 0, 1])
-        self.axes_2.set_yticks([-2, -1, 0, 1])
-        self.axes_2.set_xlabel("$x$")
-        self.axes_2.xaxis.set_label_coords(1, -0.025)
-        self.axes_2.set_ylabel("$y$")
-        self.axes_2.yaxis.set_label_coords(-0.025, 1)
+        # self.axes_2.set_xticks([-2, -1, 0, 1])
+        # self.axes_2.set_yticks([-2, -1, 0, 1])
+        # self.axes_2.set_xlabel("$x$")
+        # self.axes_2.xaxis.set_label_coords(1, -0.025)
+        # self.axes_2.set_ylabel("$y$")
+        # self.axes_2.yaxis.set_label_coords(-0.025, 1)
         self.axes2_proj_line, = self.axes_2.plot([], "*")
         self.canvas2.draw()
 
-        self.button = QtWidgets.QPushButton("Compute", self)
-        self.button.setStyleSheet("font-size:13px")
-        self.button.setGeometry(self.x_chapter_button * self.w_ratio, 500 * self.h_ratio, self.w_chapter_button * self.w_ratio, self.h_chapter_button * self.h_ratio)
-        self.button.clicked.connect(self.gram_schmidt)
-
-        self.button = QtWidgets.QPushButton("Restart", self)
-        self.button.setStyleSheet("font-size:13px")
-        self.button.setGeometry(self.x_chapter_button * self.w_ratio, 530 * self.h_ratio, self.w_chapter_button * self.w_ratio, self.h_chapter_button * self.h_ratio)
-        self.button.clicked.connect(self.clear_all)
-
-        self.label_warning = QtWidgets.QLabel(self)
-        self.label_warning.setText("")
-        self.label_warning.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_warning.setGeometry(530 * self.w_ratio, 550 * self.h_ratio, 300 * self.w_ratio, 100 * self.h_ratio)
+        self.make_button("button", "Compute", (self.x_chapter_button, 500, self.w_chapter_button, self.h_chapter_button), self.gram_schmidt)
+        self.make_button("button", "Start", (self.x_chapter_button, 530, self.w_chapter_button, self.h_chapter_button), self.clear_all)
+        self.make_label("label_warning", "", (530, 300, 300, 100))
 
     def on_mouseclick1(self, event):
         if event.xdata != None and event.xdata != None:
+            if self.text_start:
+                self.text_start.remove()
+                self.text_start = None
+            if event.xdata > 0.8 or event.xdata < -0.8:
+                event.xdata /= 2
+                event.ydata /= 2
+            if event.ydata > 0.8 or event.ydata < -0.8:
+                event.ydata /= 2
+                event.xdata /= 2
             self.axes1_points.append((round(event.xdata, 2), round(event.ydata, 2)))
             self.draw_vector()
 
@@ -95,7 +101,7 @@ class GramSchmidt(NNDLayout):
 
     def gram_schmidt(self):
         if len(self.axes1_points) < 2:
-            self.label_warning.setText("WHOOPS! Please enter 2 vectors")
+            self.label_warning.setText("WHOOPS! Please enter\n2 vectors.")
             return
         cos_angle = (self.axes1_points[0][0] * self.axes1_points[1][0] + self.axes1_points[1][0] * self.axes1_points[1][1]) / (
             np.sqrt(self.axes1_points[0][0]**2 + self.axes1_points[0][1] ** 2) * np.sqrt(self.axes1_points[1][0]**2 + self.axes1_points[1][1] ** 2)
@@ -127,10 +133,10 @@ class GramSchmidt(NNDLayout):
             self.axes2_v2.set_UVC(v2[0, 0], v2[1, 0])
             if self.text3:
                 self.text3.remove()
-            self.text3 = self.axes_2.text(v1[0, 0] * 1.2, v1[1, 0] * 1.2, "v1")
+            self.text3 = self.axes_2.text(v1[0, 0] * 1.4, v1[1, 0] * 1.4, "v1")
             if self.text4:
                 self.text4.remove()
-            self.text4 = self.axes_2.text(v2[0, 0] * 1.2, v2[1, 0], "v2")
+            self.text4 = self.axes_2.text(v2[0, 0] * 1.4, v2[1, 0] * 1.4, "v2")
         self.canvas.draw()
         self.canvas2.draw()
 
@@ -154,5 +160,6 @@ class GramSchmidt(NNDLayout):
         if self.text4:
             self.text4.remove()
             self.text4 = None
+        self.text_start = self.axes_1.text(-0.7, -0.075, "<CLICK ON ME>")
         self.canvas.draw()
         self.canvas2.draw()
