5c3a79d7865aeeb4d51dd9582e5990a46aad2774
PedroUria <43831167+PedroUria@users.noreply.github.com>
2020-03-31 09:28:16 -0400

    Demo functionality finished and design almost finished

--- nndesign/Directional_derivatives.py
+++ nndesign/Directional_derivatives.py
@@ -3,9 +3,6 @@ import numpy as np
 import warnings
 import matplotlib.cbook
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
-from matplotlib.figure import Figure
 
 from nndesign_layout import NNDLayout
 
@@ -26,19 +23,14 @@ class DirectionalDerivatives(NNDLayout):
     def __init__(self, w_ratio, h_ratio):
         super(DirectionalDerivatives, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot=False)
 
-        self.fill_chapter("Directional Derivatives", 8, "TODO",
-                          PACKAGE_PATH + "Chapters/2/Logo_Ch_2.svg", PACKAGE_PATH + "Chapters/2/nn2d1.svg", show_pic=False)
+        self.fill_chapter("Directional Derivatives", 8, "To measure a directional derivative click on the graph and move the cursor.\n\n"
+                                                        "The directional derivative is taken at the point you clicked\nin the direction of the current cursor position.\n\n"
+                                                        "Click again to choose a new point where you want\nto measure the directional derivative.",
+                          PACKAGE_PATH + "Chapters/2/Logo_Ch_2.svg", None, description_coords=(30, 400, 500, 200))
 
         self.cid = None
 
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.wid1 = QtWidgets.QWidget(self)
-        self.layout1 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid1.setGeometry(15 * self.w_ratio, 300 * self.h_ratio, 490 * self.w_ratio, 310 * self.h_ratio)
-        self.layout1.addWidget(self.canvas)
-        self.wid1.setLayout(self.layout1)
+        self.make_plot(1, (15, 120, 490, 310))
         self.axes_1 = self.figure.add_subplot(1, 1, 1)
         self.axes_1.set_title("Function F")
         self.point, = self.axes_1.plot([], marker='*')
@@ -48,24 +40,20 @@ class DirectionalDerivatives(NNDLayout):
         self.canvas.draw()
         self.canvas.mpl_connect('button_press_event', self.on_mouseclick)
 
-        self.label_dirder = QtWidgets.QLabel(self)
-        self.label_dirder.setText("Directional Derivative")
-        self.label_dirder.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_dirder.setGeometry((self.x_chapter_slider_label - 50) * self.w_ratio, 370 * self.h_ratio, 150 * self.w_ratio, 100 * self.h_ratio)
-        self.slider_dirder = QtWidgets.QSlider(QtCore.Qt.Vertical)
-        self.slider_dirder.setRange(-60, 60)
-        self.slider_dirder.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.slider_dirder.setTickInterval(1)
-        self.slider_dirder.setValue(0)
+        self.make_slider("slider_dirder", QtCore.Qt.Vertical, (-60, 60), QtWidgets.QSlider.NoTicks, 1, 0,
+                         (self.x_chapter_usual + 70, 120, self.w_chapter_slider, 310), self.freeze,
+                         "label_dirder", "Directional Derivative", (self.x_chapter_slider_label - 40, 400, 200, 100))
+        self.dir_der = 0
+        for i in range(5):
+            self.make_label("scale_{}".format(i), str(6 - i * 3) + "  -", (self.x_chapter_slider_label - 15, 125 + 62 * i, 50, 50))
 
-        self.wid3 = QtWidgets.QWidget(self)
-        self.layout3 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid3.setGeometry((self.x_chapter_usual + 30) * self.w_ratio, 430 * self.h_ratio, self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout3.addWidget(self.slider_dirder)
-        self.wid3.setLayout(self.layout3)
+    def freeze(self):
+        self.slider_dirder.setValue(self.dir_der * 10)
 
     def on_mouseclick(self, event):
         if event.xdata != None and event.xdata != None:
+            self.label_dirder.setGeometry((self.x_chapter_slider_label - 60) * self.w_ratio, 400 * self.h_ratio,
+                                          200 * self.w_ratio, 100 * self.h_ratio)
             self.point.set_data([event.xdata], [event.ydata])
             self.line_data_x, self.line_data_y = [event.xdata], [event.ydata]
             self.line.set_data(self.line_data_x, self.line_data_y)
@@ -95,5 +83,6 @@ class DirectionalDerivatives(NNDLayout):
             p = np.array([[y1 - x1], [y2 - x2]])
             grad = b + np.dot(a, xnorm)
             dir_der = np.dot(p.T, grad) / np.sqrt(np.dot(p.T, p))
-            self.slider_dirder.setValue(dir_der * 10)
-            self.label_dirder.setText("Directional Derivative: {}".format(round(dir_der.item(), 2)))
+            self.dir_der = dir_der
+            self.slider_dirder.setValue(self.dir_der * 10)
+            self.label_dirder.setText("Directional Derivative: {}".format(round(self.dir_der.item(), 2)))
