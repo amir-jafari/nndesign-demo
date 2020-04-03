fc9b6faa38364308aab57bc43ee113893e687eff
PedroUria <43831167+PedroUria@users.noreply.github.com>
2020-04-03 08:13:56 -0400

    Demo almost finished

--- nndesign/Variable_learning_rate.py
+++ nndesign/Variable_learning_rate.py
@@ -43,25 +43,21 @@ class VariableLearningRate(NNDLayout):
     def __init__(self, w_ratio, h_ratio):
         super(VariableLearningRate, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot=False)
 
-        self.fill_chapter("Variable Learning Rate", 9, " Click anywhere to start an\n initial guess. The gradient\n descent path will be shown\n"
-                                                               " Modify the learning rate\n by moving the slide bar",
-                          PACKAGE_PATH + "Chapters/2/Logo_Ch_2.svg", PACKAGE_PATH + "Chapters/2/nn2d1.svg", show_pic=False)  # TODO: Change icons
+        self.fill_chapter("Variable Learning Rate", 12, "\nUse the radio buttons to\nselect the network\nparameters"
+                                                        " to train\nwith backpropagation.\n\nThe corresponding contour\nplot "
+                                                        "is shown below.\n\nClick in the contour graph\nto start "
+                                                        "the variable learning\nrate backprop algorithm.",
+                          PACKAGE_PATH + "Logo/Logo_Ch_12.svg", None, description_coords=(535, 90, 450, 300))
 
         self.W1, self.b1 = np.array([[10], 10]), np.array([[-5], [5]])
         self.W2, self.b2 = np.array([[1, 1]]), np.array([[-1]])
         self.lr, self.epochs = None, None
 
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.wid1 = QtWidgets.QWidget(self)
-        self.layout1 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid1.setGeometry(50 * self.w_ratio, 200 * self.h_ratio, 450 * self.w_ratio, 450 * self.h_ratio)
-        self.layout1.addWidget(self.canvas)
-        self.wid1.setLayout(self.layout1)
-
+        self.make_plot(1, (20, 200, 480, 480))
         self.axes = self.figure.add_subplot(1, 1, 1)
         self.path, = self.axes.plot([], linestyle='--', marker='*', label="Gradient Descent Path")
+        self.init_point_1, = self.axes.plot([], "o", fillstyle="none", markersize=11, color="k")
+        self.end_point_1, = self.axes.plot([], "o", fillstyle="none", markersize=11, color="k")
         self.x_data, self.y_data = [], []
         self.canvas.draw()
         self.canvas.mpl_connect('button_press_event', self.on_mouseclick)
@@ -73,95 +69,32 @@ class VariableLearningRate(NNDLayout):
 
         self.x, self.y = None, None
 
-        self.comboBox1 = QtWidgets.QComboBox(self)
-        self.comboBox1.addItems(["W1(1, 1), W2(1, 1)", 'W1(1, 1), b1(1)', 'b1(1), b1(2)'])
-        self.label_combo = QtWidgets.QLabel(self)
-        self.label_combo.setText("Pair of parameters")
-        self.label_combo.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_combo.setGeometry((self.x_chapter_slider_label + 10) * self.w_ratio, 550 * self.h_ratio,
-                                     150 * self.w_ratio, 100 * self.h_ratio)
-        self.wid2 = QtWidgets.QWidget(self)
-        self.layout2 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid2.setGeometry(self.x_chapter_usual * self.w_ratio, 580 * self.h_ratio,
-                              self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout2.addWidget(self.comboBox1)
-        self.wid2.setLayout(self.layout2)
+        self.make_combobox(1, ["W1(1, 1), W2(1, 1)", 'W1(1, 1), b1(1)', 'b1(1), b1(2)'],
+                           (525, 360, 150, 50), self.change_pair_of_params,
+                           "label_combo", "Pair of parameters", (545, 340, 150, 50))
 
         self.lr = 14
-        self.label_lr = QtWidgets.QLabel(self)
-        self.label_lr.setText("lr: 14")
-        self.label_lr.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_lr.setGeometry(self.x_chapter_slider_label * self.w_ratio, 250 * self.h_ratio, self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.slider_lr = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.slider_lr.setRange(0, 200)
-        self.slider_lr.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.slider_lr.setTickInterval(1)
-        self.slider_lr.setValue(140)
-        self.wid_lr = QtWidgets.QWidget(self)
-        self.layout_lr = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid_lr.setGeometry(self.x_chapter_usual * self.w_ratio, 280 * self.h_ratio, self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout_lr.addWidget(self.slider_lr)
-        self.wid_lr.setLayout(self.layout_lr)
+        self.make_label("label_lr1", "0.0", (self.x_chapter_usual + 10, 610, self.w_chapter_slider, 50))
+        self.make_label("label_lr2", "20.0", (self.x_chapter_usual + 150, 610, self.w_chapter_slider, 50))
+        self.make_slider("slider_lr", QtCore.Qt.Horizontal, (0, 200), QtWidgets.QSlider.TicksBelow, 1, 140,
+                         (self.x_chapter_usual, 580, self.w_chapter_slider, 50), self.slide, "label_lr", "lr: 14.0")
 
         self.increase_rate = 1.05
-        self.label_increase = QtWidgets.QLabel(self)
-        self.label_increase.setText("Increase rate: 1.05")
-        self.label_increase.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_increase.setGeometry(self.x_chapter_slider_label * self.w_ratio, 320 * self.h_ratio,
-                                        self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.slider_increase = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.slider_increase.setRange(100, 120)
-        self.slider_increase.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.slider_increase.setTickInterval(1)
-        self.slider_increase.setValue(105)
-        self.wid_increase = QtWidgets.QWidget(self)
-        self.layout_increase = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid_increase.setGeometry(self.x_chapter_usual * self.w_ratio, 350 * self.h_ratio,
-                                      self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout_increase.addWidget(self.slider_increase)
-        self.wid_increase.setLayout(self.layout_increase)
+        self.make_label("label_increase1", "1.00", (self.x_chapter_usual + 10, 460, self.w_chapter_slider, 50))
+        self.make_label("label_increase2", "1.20", (self.x_chapter_usual + 150, 460, self.w_chapter_slider, 50))
+        self.make_slider("slider_increase", QtCore.Qt.Horizontal, (100, 120), QtWidgets.QSlider.TicksBelow, 1, 105,
+                         (self.x_chapter_usual, 430, self.w_chapter_slider, 50), self.slide, "label_increase", "Increase rate: 1.05",
+                         (self.x_chapter_usual + 50, 400, self.w_chapter_slider, 50))
 
         self.decrease_rate = 0.7
-        self.label_decrease = QtWidgets.QLabel(self)
-        self.label_decrease.setText("Decrease rate: 0.7")
-        self.label_decrease.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_decrease.setGeometry(self.x_chapter_slider_label * self.w_ratio, 390 * self.h_ratio,
-                                        self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.slider_decrease = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.slider_decrease.setRange(50, 100)
-        self.slider_decrease.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.slider_decrease.setTickInterval(1)
-        self.slider_decrease.setValue(70)
-        self.wid_decrease = QtWidgets.QWidget(self)
-        self.layout_decrease = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid_decrease.setGeometry(self.x_chapter_usual * self.w_ratio, 420 * self.h_ratio,
-                                      self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout_decrease.addWidget(self.slider_decrease)
-        self.wid_decrease.setLayout(self.layout_decrease)
+        self.make_label("label_decrease1", "0.50", (self.x_chapter_usual + 10, 540, self.w_chapter_slider, 50))
+        self.make_label("label_decrease2", "1.00", (self.x_chapter_usual + 150, 540, self.w_chapter_slider, 50))
+        self.make_slider("slider_decrease", QtCore.Qt.Horizontal, (50, 100), QtWidgets.QSlider.TicksBelow, 1, 70,
+                         (self.x_chapter_usual, 510, self.w_chapter_slider, 50), self.slide, "label_decrease",
+                         "Decrease rate: 0.70", (self.x_chapter_usual + 50, 480, self.w_chapter_slider, 50))
 
-        self.animation_speed = 100
-        self.label_anim_speed = QtWidgets.QLabel(self)
-        self.label_anim_speed.setText("Animation Delay: 100 ms")
-        self.label_anim_speed.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_anim_speed.setGeometry((self.x_chapter_slider_label - 40) * self.w_ratio, 460 * self.h_ratio,
-                                          self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.slider_anim_speed = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.slider_anim_speed.setRange(0, 6)
-        self.slider_anim_speed.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.slider_anim_speed.setTickInterval(1)
-        self.slider_anim_speed.setValue(1)
-        self.wid_anim_speed = QtWidgets.QWidget(self)
-        self.layout_anim_speed = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid_anim_speed.setGeometry(self.x_chapter_usual * self.w_ratio, 490 * self.h_ratio,
-                                        self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout_anim_speed.addWidget(self.slider_anim_speed)
-        self.wid_anim_speed.setLayout(self.layout_anim_speed)
+        self.animation_speed = 0
 
-        self.comboBox1.currentIndexChanged.connect(self.change_pair_of_params)
-        self.slider_lr.valueChanged.connect(self.slide)
-        self.slider_increase.valueChanged.connect(self.slide)
-        self.slider_decrease.valueChanged.connect(self.slide)
-        self.slider_anim_speed.valueChanged.connect(self.slide)
         self.canvas.draw()
 
         self.dW1, self.db1, self.dW2, self.db2 = 0, 0, 0, 0
@@ -186,22 +119,22 @@ class VariableLearningRate(NNDLayout):
         if self.pair_of_params == 1:
             self.axes.set_xlim(-5, 15)
             self.axes.set_ylim(-5, 15)
-            self.axes.set_xticks([-5, 0, 5, 10])
-            self.axes.set_yticks([-5, 0, 5, 10])
+            self.axes.set_xticks([-5, 0, 5, 10, 15])
+            self.axes.set_yticks([-5, 0, 5, 10, 15])
         elif self.pair_of_params == 2:
             self.axes.set_xlim(-10, 30)
             self.axes.set_ylim(-20, 10)
-            self.axes.set_xticks([-10, 0, 10, 20])
-            self.axes.set_yticks([-20, -15, -10, -5, 0, 5])
+            self.axes.set_xticks([-10, 0, 10, 20, 30])
+            self.axes.set_yticks([-20, -15, -10, -5, 0, 5, 10])
         elif self.pair_of_params == 3:
             self.axes.set_xlim(-10, 10)
             self.axes.set_ylim(-10, 10)
-            self.axes.set_xticks([-10, -5, 0, 5])
-            self.axes.set_xticks([-10, -5, 0, 5])
+            self.axes.set_xticks([-10, -5, 0, 5, 10])
+            self.axes.set_xticks([-10, -5, 0, 5, 10])
         self.axes.set_xlabel(self.pair_params[self.pair_of_params - 1][0], fontsize=8)
-        self.axes.xaxis.set_label_coords(0.95, -0.025)
+        # self.axes.xaxis.set_label_coords(0.95, -0.025)
         self.axes.set_ylabel(self.pair_params[self.pair_of_params - 1][1], fontsize=8)
-        self.axes.yaxis.set_label_coords(-0.025, 0.95)
+        # self.axes.yaxis.set_label_coords(-0.025, 0.95)
         self.canvas.draw()
 
     def slide(self):
@@ -212,8 +145,8 @@ class VariableLearningRate(NNDLayout):
             self.label_increase.setText("Increase rate: " + str(self.increase_rate))
             self.decrease_rate = float(self.slider_decrease.value() / 100)
             self.label_decrease.setText("Decrease rate: " + str(self.decrease_rate))
-            self.animation_speed = int(self.slider_anim_speed.value()) * 100
-            self.label_anim_speed.setText("Animation Delay: " + str(self.animation_speed) + " ms")
+            # self.animation_speed = int(self.slider_anim_speed.value()) * 100
+            # self.label_anim_speed.setText("Animation Delay: " + str(self.animation_speed) + " ms")
             if self.x_data:
                 if self.ani:
                     self.ani.event_source.stop()
@@ -233,7 +166,7 @@ class VariableLearningRate(NNDLayout):
         self.e = T - self.a2
         self.D2 = self.a2 * (1 - self.a2) * self.e
         self.D1 = self.a1 * (1 - self.a1) * np.dot(self.W2.T, self.D2)
-        return self.path,
+        return self.path, self.end_point_1
 
     def on_animate(self, idx):
 
@@ -282,11 +215,13 @@ class VariableLearningRate(NNDLayout):
             self.D2 = self.a2 * (1 - self.a2) * self.e
             self.D1 = self.a1 * (1 - self.a1) * np.dot(self.W2.T, self.D2)
 
+        self.end_point_1.set_data([self.x_data[-1], self.y_data[-1]])
+
         self.x_data.append(self.x)
         self.y_data.append(self.y)
         self.path.set_data(self.x_data, self.y_data)
         self.slider_do = True
-        return self.path,
+        return self.path, self.end_point_1
 
     def on_mouseclick(self, event):
         self.init_params()
@@ -295,6 +230,7 @@ class VariableLearningRate(NNDLayout):
             self.ani.event_source.stop()
         self.path.set_data([], [])
         self.x_data, self.y_data = [], []
+        self.init_point_1.set_data([event.xdata], [event.ydata])
         self.canvas.draw()
         self.run_animation(event)
 
