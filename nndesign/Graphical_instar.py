d0a2198c9840d9f3bb194923cc2dc31f1b7d5485
PedroUria <43831167+PedroUria@users.noreply.github.com>
2020-04-05 13:46:05 -0400

    Design done and demo finished

--- nndesign/Graphical_instar.py
+++ nndesign/Graphical_instar.py
@@ -9,15 +9,20 @@ from nndesign_layout import NNDLayout
 
 from get_package_path import PACKAGE_PATH
 
-# Figured out why I was having so much trouble with the quiver scaling. It was because the plot was now a square...!!
-
 
 class GraphicalInstar(NNDLayout):
     def __init__(self, w_ratio, h_ratio):
-        super(GraphicalInstar, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot=False, create_two_plots=True)
+        super(GraphicalInstar, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot=False)
 
-        self.fill_chapter("Graphical Instar", 15, "TODO",
-                          PACKAGE_PATH + "Logo/Logo_Ch_5.svg", PACKAGE_PATH + "Chapters/2/nn2d1.svg", show_pic=False)
+        self.fill_chapter("Graphical Instar", 15, "Click on the top graph\nto change the green\nweight vector.\n\n"
+                                                  "Click on the bottom graph\nto change the red\ninput vector.\n\n"
+                                                  "Use the slider to\nchange the learning rate.\n\n"
+                                                  "The change in the weight\nvector is shown with\na black arrow.\n\n"
+                                                  "Click [Update] to\nmake the change.",
+                          PACKAGE_PATH + "Logo/Logo_Ch_15.svg", None, description_coords=(535, 140, 450, 300))
+
+        self.make_plot(1, (120, 120, 270, 270))
+        self.make_plot(2, (120, 390, 270, 270))
 
         self.axes_1 = self.figure.add_subplot(1, 1, 1)
         self.axes_1.set_title("Click to change weight")
@@ -27,27 +32,15 @@ class GraphicalInstar(NNDLayout):
         self.axes1_w = self.axes_1.quiver([0], [0], [self.w[0, 0]], [self.w[0, 1]], units="xy", scale=1, color="green")
         self.axes_1.plot([0] * 6, np.linspace(-1.1, 1.1, 6), color="black", linestyle="--", linewidth=0.2)
         self.axes_1.plot(np.linspace(-1.1, 1.1, 6), [0] * 6, color="black", linestyle="--", linewidth=0.2)
+        self.axes_1.set_xticks([], [])
+        self.axes_1.set_yticks([], [])
         self.canvas.draw()
         self.canvas.mpl_connect('button_press_event', self.on_mouseclick1)
 
         self.lr = 0.5
-        self.label_lr = QtWidgets.QLabel(self)
-        self.label_lr.setText("Learning rate: 0.5")
-        self.label_lr.setFont(QtGui.QFont("Times New Roman", 12, italic=True))
-        self.label_lr.setGeometry(self.x_chapter_slider_label * self.w_ratio, 400 * self.h_ratio, 150 * self.w_ratio,
-                                  100 * self.h_ratio)
-        self.slider_lr = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.slider_lr.setRange(0, 10)
-        self.slider_lr.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.slider_lr.setTickInterval(1)
-        self.slider_lr.setValue(5)
-        self.wid3 = QtWidgets.QWidget(self)
-        self.layout3 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid3.setGeometry(self.x_chapter_usual * self.w_ratio, 430 * self.h_ratio,
-                              self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout3.addWidget(self.slider_lr)
-        self.wid3.setLayout(self.layout3)
-        self.slider_lr.valueChanged.connect(self.slide)
+        self.make_slider("slider_lr", QtCore.Qt.Horizontal, (0, 10), QtWidgets.QSlider.TicksBelow, 1, 5,
+                         (self.x_chapter_usual, 470, self.w_chapter_slider, 50), self.slide,
+                         "label_lr", "Learning Rate: 0.5", (self.x_chapter_slider_label - 30, 440, 150, 50))
 
         self.axes_2 = self.figure2.add_subplot(1, 1, 1)
         self.axes_2.set_title("Click to change input")
@@ -60,14 +53,13 @@ class GraphicalInstar(NNDLayout):
         self.axes2_line, = self.axes_2.plot([], color="black", linewidth=0.5)
         self.axes_2.plot([0] * 6, np.linspace(-1.1, 1.1, 6), color="black", linestyle="--", linewidth=0.2)
         self.axes_2.plot(np.linspace(-1.1, 1.1, 6), [0] * 6, color="black", linestyle="--", linewidth=0.2)
+        self.axes_2.set_xticks([], [])
+        self.axes_2.set_yticks([], [])
         self.compute()
         self.canvas2.draw()
         self.canvas2.mpl_connect('button_press_event', self.on_mouseclick2)
 
-        self.button = QtWidgets.QPushButton("Update", self)
-        self.button.setStyleSheet("font-size:13px")
-        self.button.setGeometry(self.x_chapter_button * self.w_ratio, 600 * self.h_ratio, self.w_chapter_button * self.w_ratio, self.h_chapter_button * self.h_ratio)
-        self.button.clicked.connect(self.update)
+        self.make_button("button", "Update", (self.x_chapter_button, 520, self.w_chapter_button, self.h_chapter_button), self.update)
 
     def slide(self):
         self.lr = self.slider_lr.value() / 10
