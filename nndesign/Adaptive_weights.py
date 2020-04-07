46beee0f5f4ec06ba908055184cbc65aa480a900
PedroUria <43831167+PedroUria@users.noreply.github.com>
2020-04-07 17:03:10 -0400

    Fixes and design done

--- nndesign/Adaptive_weights.py
+++ nndesign/Adaptive_weights.py
@@ -1,5 +1,3 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
-import math
 import numpy as np
 import warnings
 import matplotlib.cbook
@@ -11,97 +9,67 @@ from nndesign_layout import NNDLayout
 from get_package_path import PACKAGE_PATH
 
 
-t = np.arange(0, 2, 0.1)
+t = np.arange(0, 2, 0.01)
 
 
 class AdaptiveWeights(NNDLayout):
     def __init__(self, w_ratio, h_ratio):
-        super(AdaptiveWeights, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot_coords=(25, 90, 450, 450))
+        super(AdaptiveWeights, self).__init__(w_ratio, h_ratio, main_menu=1, create_plot=False)
 
-        self.fill_chapter("Grossberg Layer 1", 2, " TODO",
-                          PACKAGE_PATH + "Chapters/2/Logo_Ch_2.svg", PACKAGE_PATH + "Chapters/2/nn2d1.svg", show_pic=False)
+        self.fill_chapter("Adaptive Weights", 18, "\n\n\nEdit the two input\nvectors and click\n[Update] to see the\nnetwork "
+                                                  "learn them.\n\nW2(1,1) - solid red\nW2(1,2) - broken red\nW2(2,1) -"
+                                                  " solid green\nW2(2,2) - broken green\n\nClick [Clear] to remove\n"
+                                                  "old responses.",
+                          PACKAGE_PATH + "Logo/Logo_Ch_18.svg", None)
+
+        self.make_plot(1, (10, 90, 500, 450))
+        self.figure.subplots_adjust(left=0.15, right=0.95, bottom=0.125, top=0.9)
 
         self.axis = self.figure.add_subplot(1, 1, 1)
         self.axis.set_xlim(-0.1, 2.1)
         self.axis.set_ylim(-0.1, 1.1)
         for i in [0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2]:
-            self.axis.plot([i] * 10, np.linspace(-0.1, 1.1, 10), color="black", linestyle="dashed", linewidth=0.2)
+            self.axis.plot([i] * 10, np.linspace(-0.1, 1.1, 10), color="black", linestyle="--", linewidth=0.5)
+            if i in [0, 0.4, 0.8, 1.2, 1.6, 2]:
+                if i != 2:
+                    self.axis.text(i + 0.0175, -0.075, "1st")
+            else:
+                self.axis.text(i + 0.015, -0.075, "2nd")
         self.axis.set_yticks([0, 0.25, 0.5, 0.75, 1])
         self.axis.set_xlabel("Time")
         self.axis.set_ylabel("Weights W2")
         self.axis.set_title("Learning")
+        self.axis.set_xticks([0, 0.5, 1, 1.5, 2])
         self.lines1, self.lines2, self.lines3, self.lines4 = [], [], [], []
 
-        self.n_11 = QtWidgets.QLineEdit()
-        self.n_11.setText("0.9")
-        self.n_11.setGeometry(50 * self.w_ratio, 530 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.wid3 = QtWidgets.QWidget(self)
-        self.layout3 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid3.setGeometry(50 * self.w_ratio, 530 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.layout3.addWidget(self.n_11)
-        self.wid3.setLayout(self.layout3)
+        self.paint_latex_string("latex_n11", "$1st$", 10, (20, 510, 500, 200))
+        self.paint_latex_string("latex_n12", "$n1 =$", 10, (75, 510, 500, 200))
+        self.paint_latex_string("latex_n13", "$[$", 40, (140, 510, 500, 200))
+        self.paint_latex_string("latex_n14", "$]$", 40, (200, 510, 500, 200))
+        self.make_input_box("n_11", "0.9", (160, 537, 70, 100))
+        self.make_input_box("n_12", "0.45", (160, 577, 70, 100))
 
-        self.n_12 = QtWidgets.QLineEdit()
-        self.n_12.setText("0.45")
-        self.n_12.setGeometry(50 * self.w_ratio, 600 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.wid3 = QtWidgets.QWidget(self)
-        self.layout3 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid3.setGeometry(50 * self.w_ratio, 600 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.layout3.addWidget(self.n_12)
-        self.wid3.setLayout(self.layout3)
+        self.paint_latex_string("latex_n21", "$2nd$", 10, (270, 510, 500, 200))
+        self.paint_latex_string("latex_n22", "$n1 =$", 10, (335, 510, 500, 200))
+        self.paint_latex_string("latex_n23", "$[$", 40, (400, 510, 500, 200))
+        self.paint_latex_string("latex_n24", "$]$", 40, (460, 510, 500, 200))
+        self.make_input_box("n_21", "0.45", (420, 537, 70, 100))
+        self.make_input_box("n_22", "0.90", (420, 577, 70, 100))
 
-        self.n_21 = QtWidgets.QLineEdit()
-        self.n_21.setText("0.45")
-        self.n_21.setGeometry(300 * self.w_ratio, 530 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.wid3 = QtWidgets.QWidget(self)
-        self.layout3 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid3.setGeometry(300 * self.w_ratio, 530 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.layout3.addWidget(self.n_21)
-        self.wid3.setLayout(self.layout3)
-
-        self.n_22 = QtWidgets.QLineEdit()
-        self.n_22.setText("0.9")
-        self.n_22.setGeometry(300 * self.w_ratio, 600 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.wid3 = QtWidgets.QWidget(self)
-        self.layout3 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid3.setGeometry(300 * self.w_ratio, 600 * self.h_ratio, 75 * self.w_ratio, 100 * self.h_ratio)
-        self.layout3.addWidget(self.n_22)
-        self.wid3.setLayout(self.layout3)
-
-        self.comboBox1 = QtWidgets.QComboBox(self)
         self.comboBox1_functions_str = ['Instar', 'Hebb']
-        self.comboBox1.addItems(self.comboBox1_functions_str)
+        self.make_combobox(1, self.comboBox1_functions_str, (self.x_chapter_usual, 460, self.w_chapter_slider, 50),
+                           self.change_learning_rule, "combobox1_label", "Learning rule",
+                           (self.x_chapter_usual + 20, 460 - 20, 100, 50))
         self.rule = 1
-        self.label_f = QtWidgets.QLabel(self)
-        self.label_f.setText("Learning rule")
-        self.label_f.setFont(QtGui.QFont("Times New Roman", 12))
-        self.label_f.setGeometry(self.x_chapter_slider_label * self.w_ratio, 480 * self.h_ratio,
-                                 150 * self.w_ratio, 100 * self.h_ratio)
-        self.wid2 = QtWidgets.QWidget(self)
-        self.layout2 = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
-        self.wid2.setGeometry(self.x_chapter_usual * self.w_ratio, 500 * self.h_ratio,
-                              self.w_chapter_slider * self.w_ratio, 100 * self.h_ratio)
-        self.layout2.addWidget(self.comboBox1)
-        self.wid2.setLayout(self.layout2)
-        self.comboBox1.currentIndexChanged.connect(self.change_learning_rule)
 
-        self.clear_button = QtWidgets.QPushButton("Clear", self)
-        self.clear_button.setStyleSheet("font-size:13px")
-        self.clear_button.setGeometry(self.x_chapter_button * self.w_ratio, 580 * self.h_ratio,
-                                      self.w_chapter_button * self.w_ratio, self.h_chapter_button * self.h_ratio)
-        self.clear_button.clicked.connect(self.on_clear)
-
-        self.run_button = QtWidgets.QPushButton("Update", self)
-        self.run_button.setStyleSheet("font-size:13px")
-        self.run_button.setGeometry(self.x_chapter_button * self.w_ratio, 420 * self.h_ratio,
-                                    self.w_chapter_button * self.w_ratio, self.h_chapter_button * self.h_ratio)
-        self.run_button.clicked.connect(self.graph)
+        self.make_button("clear_button", "Clear", (self.x_chapter_button, 560, self.w_chapter_button, self.h_chapter_button), self.on_clear)
+        self.make_button("run_button", "Update", (self.x_chapter_button, 585, self.w_chapter_button, self.h_chapter_button), self.graph)
 
         self.graph()
 
     def change_learning_rule(self, idx):
         self.rule = idx + 1
-        self.graph()
+        # self.graph()
 
     def adapt(self, t, w):
         if np.fix(t / 0.2) % 2 == 0:
@@ -124,14 +92,15 @@ class AdaptiveWeights(NNDLayout):
         r1 = ode(self.adapt).set_integrator("zvode")
         r1.set_initial_value(np.zeros((4,)), 0)
         t1 = 2
-        dt = 0.1
+        dt = 0.01
         out_11, out_21, out_12, out_22 = [], [], [], []
         while r1.successful() and r1.t < t1:
             out = r1.integrate(r1.t + dt)
-            out_11.append(out[0])
-            out_12.append(out[1])
-            out_21.append(out[2])
-            out_22.append(out[3])
+            out_11.append(out[0].item())
+            out_12.append(out[1].item())
+            out_21.append(out[2].item())
+            out_22.append(out[3].item())
+        out_11[0], out_12[0], out_21[0], out_22[0] = 0, 0, 0, 0
         while len(self.lines1) > 1:
             self.lines1.pop(0).remove()
         while len(self.lines2) > 1:
